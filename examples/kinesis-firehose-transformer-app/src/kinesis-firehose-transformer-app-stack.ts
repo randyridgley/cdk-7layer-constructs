4bbac85f1245f6179595df4a1c1e23eb1fce3f46
Ridgley, Randy <22918922+randyridgley@users.noreply.github.com>
2020-08-04 16:25:40 -0400

    update deps and config

--- examples/kinesis-firehose-transformer-app/src/kinesis-firehose-transformer-app-stack.ts
+++ examples/kinesis-firehose-transformer-app/src/kinesis-firehose-transformer-app-stack.ts
@@ -1,10 +1,34 @@
-import { Construct, Stack, StackProps } from '@aws-cdk/core';
-
+import { Construct, Stack, StackProps, RemovalPolicy } from '@aws-cdk/core';
+import { KinesisFirehoseTransformer } from '@cdk-7layer-constructs/kinesis-firehose-transformer';
+import { RetentionDays } from '@aws-cdk/aws-logs'
 
 export class KinesisFirehoseTransformerAppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new KinesisFirehoseTransformer(this, 'KinesisConverter', {
+      createEncryptionKey: true,
+      deliveryStreamName: 'test-delivery',
+      enableCloudwatchLogging: true,
+      targetTableConfig: {
+        columns: [],
+        glueDatabaseArn: "aws:::glue:database",
+        tableName: 'testTable',
+        targetS3BucketArn: 'arn:::s3:bucket',
+        targetS3prefix: 'processed/'
+      },
+      logsConfig: {
+        logsGroupName: '/aws/kinesisfirehose/test-delivery',
+        logsRemovalPolicy: RemovalPolicy.DESTROY,
+        logsRetentionDays: RetentionDays.ONE_WEEK
+      },
+      sourceBackupConfig: {
+        columns: [],
+        glueDatabaseArn: "aws:::glue:database",
+        tableName: 'testTable',
+        targetS3BucketArn: 'arn:::s3:bucket',
+        targetS3prefix: 'raw/'
+      }
+    })
   }
 }
