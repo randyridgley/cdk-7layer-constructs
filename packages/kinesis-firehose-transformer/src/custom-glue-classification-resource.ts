43c94e776dfc27e80259ff1a6c0d6b5a2f4d4802
Ridgley, Randy <22918922+randyridgley@users.noreply.github.com>
2020-08-11 21:27:07 -0400

    chore(release): publish independent

--- packages/kinesis-firehose-transformer/src/custom-glue-classification-resource.ts
+++ packages/kinesis-firehose-transformer/src/custom-glue-classification-resource.ts
@@ -1,7 +1,8 @@
-import * as cfn from '@aws-cdk/aws-cloudformation'
 import * as lambda from '@aws-cdk/aws-lambda'
 import * as cdk from '@aws-cdk/core'
+import * as cfn from '@aws-cdk/aws-cloudformation';
 import * as iam  from '@aws-cdk/aws-iam'
+import * as customResource from '@aws-cdk/custom-resources'
 import path = require("path");
 
 export interface CustomGlueClassificationResourceProps {
@@ -16,8 +17,8 @@ export class CustomGlueClassificationResource extends cdk.Construct {
     super(scope, id);
 
     new cfn.CustomResource(this, "GlueClassification", {
-      provider: cfn.CustomResourceProvider.lambda(
-        new lambda.SingletonFunction(this, "CustomGlueClassificationResourceFunction", {
+      provider: new customResource.Provider(this, 'GlueClassificationUpdate', {
+        onEventHandler: new lambda.SingletonFunction(this, "CustomGlueClassificationResourceFunction", {
           uuid: 'd8d85062-7bd5-41bc-a1f7-4c00e4fbeac7',
           code: lambda.Code.fromAsset(
             path.join(__dirname, "lambdas/custom-glue-classification-resource")
@@ -26,25 +27,9 @@ export class CustomGlueClassificationResource extends cdk.Construct {
           timeout: cdk.Duration.seconds(30),
           runtime: lambda.Runtime.NODEJS_12_X,
           role: iam.Role.fromRoleArn(this, 'CustomRole', props.roleArn)
-        })
-      ),
+        })      
+      }),
       properties: props
     });
   }
-}
-
-// hack uuid not installing correctly just need unique string for lambda custom resources
-export function uuid() :string {
-  return `${randomString(8)}-${randomString(4)}-${randomString(4)}-${randomString(4)}-${randomString(12)}`
-}
-
-export function randomString(length: number = 10, alphabet: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'): string {
-  let text = '';
-
-  for (let i = 0; i < length; i++) {
-    text =
-      text + alphabet.charAt(Math.floor(Math.random() * alphabet.length));
-  }
-
-  return text;
 }
